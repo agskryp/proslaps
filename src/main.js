acc48364e415cdeefa690855c8ad1d651dcba6f7
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-13 15:52:50 -0500

    Import necessary icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faHandPaper,
+  faHandRock,
+  faHandScissors
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHandPaper, faHandRock, faHandScissors);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
