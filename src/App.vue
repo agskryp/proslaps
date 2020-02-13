92c4888a14af79259dd799809119228b42dc65cf
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-13 15:53:03 -0500

    Basic items + layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Paper, Rock, Scissors</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div>Human Icon</div>
+          <font-awesome-icon icon="hand-paper" />
+          <font-awesome-icon icon="hand-rock" />
+          <font-awesome-icon icon="hand-scissors" />
+
+          <br />
+
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="button"
+            aria-pressed="false"
+          >
+            Paper
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="button"
+            aria-pressed="false"
+          >
+            Rock
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="button"
+            aria-pressed="false"
+          >
+            Scissors
+          </button>
+        </div>
+        <div class="col">
+          <div>Computer Icon</div>
+
+          <br />
+
+          <div>Is thinking...</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          Wins: #
+        </div>
+
+        <div class="col">
+          Draws: #
+        </div>
+
+        <div class="col">
+          Loses: #
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          Made by A.G. Skryp
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
+  //  components: {
+  //    HelloWorld
+  //  }
 };
 </script>
 
