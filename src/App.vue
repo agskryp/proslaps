80884d480a136446b77b9d4b3506b49aaadb7048
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-14 14:33:38 -0500

    Set battle rules

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,15 @@
       </div>
       <div class="row">
         <div class="col">
-<!--
-          <font-awesome-icon size="5x" icon="hand-paper" />
-          <font-awesome-icon size="5x" icon="hand-rock" />
-          <font-awesome-icon size="5x" icon="hand-scissors" />
--->
           <font-awesome-icon size="5x" :icon="this.humanChoice" class="human-icon" />
 
-          <br />
-
+          <div>
           <button
             type="button"
             class="btn btn-primary"
             data-toggle="button"
             aria-pressed="false"
+            @click="onAction('hand-paper')"
           >
             Paper
           </button>
@@ -31,6 +26,7 @@
             class="btn btn-primary"
             data-toggle="button"
             aria-pressed="false"
+            @click="onAction('hand-rock')"
           >
             Rock
           </button>
@@ -40,14 +36,14 @@
             class="btn btn-primary"
             data-toggle="button"
             aria-pressed="false"
+            @click="onAction('hand-scissors')"
           >
             Scissors
           </button>
+  </div>
         </div>
         <div class="col">
           <font-awesome-icon class="computer-icon" size="5x" :icon="this.ComputerChoice" />
-
-          <br />
 
           <div>Is thinking...</div>
         </div>
@@ -87,6 +83,7 @@ export default {
 
   data() {
     return {
+      idleScreen: true,
       computerIconGenerator: 1,
       humanIconGenerator: 1,
       ComputerChoice: "hand-scissors",
@@ -95,11 +92,13 @@ export default {
   },
 
   mounted() {
-    this.animate();
+//    this.onIdleScreen();
   },
 
   methods: {
-    animate() {
+    onIdleScreen() {
+      this.idleScreen = true;
+      
       setInterval(() => {
         this.computerIconGenerator = Math.floor(Math.random() * 3 + 1);
         this.humanIconGenerator = Math.floor(Math.random() * 3 + 1);
@@ -128,7 +127,57 @@ export default {
           this.humanChoice = "hand-scissors";
         }
       }, 1000);
-    }
+    },
+    
+    onAction( hand ) {
+      this.idleScreen = false;
+      
+      this.computerIconGenerator = Math.floor(Math.random() * 3 + 1);
+      
+      if (this.computerIconGenerator == 1) {
+        this.ComputerChoice = "hand-paper";
+      }
+
+      if (this.computerIconGenerator == 2) {
+        this.ComputerChoice = "hand-rock";
+      }
+
+      if (this.computerIconGenerator == 3) {
+        this.ComputerChoice = "hand-scissors";
+      }
+      
+      console.log( hand );
+      console.log( this.ComputerChoice );
+      
+      if( hand == 'hand-rock' && this.ComputerChoice == 'hand-scissors' || 
+          hand == 'hand-paper' && this.ComputerChoice == 'hand-rock' ||
+          hand == 'hand-scissors' && this.ComputerChoice == 'hand-paper' ) {
+        console.log( 'User wins!' );
+      } else if( hand == 'hand-rock' && this.ComputerChoice == 'hand-rock' || 
+          hand == 'hand-paper' && this.ComputerChoice == 'hand-paper' ||
+          hand == 'hand-scissors' && this.ComputerChoice == 'hand-scissors' ) {
+        console.log( 'It\'s a draw!' );
+      }
+      else if( hand == 'hand-rock' && this.ComputerChoice == 'hand-paper' || 
+          hand == 'hand-paper' && this.ComputerChoice == 'hand-scissors' ||
+          hand == 'hand-scissors' && this.ComputerChoice == 'hand-rock' ) {
+        console.log( 'Computer wins' );
+      }
+      
+      
+      
+      
+      
+//      setTimeout( () => {
+//        console.log( 'Action has ended' );
+//      }, 4000);
+//      
+//      
+      
+      
+      
+      this.idleScreen = true;
+    },
   },
 
   created: function() {}
