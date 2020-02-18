c5a0a72e603ead4fded5d949235ea3537eb04771
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-18 15:07:51 -0500

    Config vue file

---
+++ vue.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: ['/Users/ag87/_WebDev/_GitHubProjects/paper-rock-scissor/src/styles/main.scss']
+    }
+  },
+
+  configureWebpack: {
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        minSize: 0,
+        maxInitialRequests: Infinity,
+        chunks: "all",
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace("@", "")}`;
+            }
+          }
+        }
+      }
+    },
+    devtool: false,
+
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith(".js", ".css");
+      }
+    }
+  }
+};
