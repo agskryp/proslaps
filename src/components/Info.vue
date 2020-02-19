f223ca8e8016c9fa8bf7b8f3a638b4524b8e721f
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-19 16:36:13 -0500

    Code clean up

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,34 +1,47 @@
 <template>
   <div class="modal-container">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-              @click="closeInfo()"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-                  <p><strong>Pro Slaps!</strong> is an extension of the traditional <b>Ro</b>ck, <b>Pa</b>per, <b>Sc</b>issors game, with <b>L</b>izard, and <b>Sp</b>ock being additional choices.</p>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-              @click="closeInfo()"
-            >
-              Close
-            </button>
-          </div>
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Information</h5>
+          <button
+            type="button"
+            class="close"
+            aria-label="Close"
+            @click="closeInfo()"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h4>What is Pro Slaps?</h4>
+          
+          <p>Pro Slaps! is an extension of the traditional Rock, Paper, Scissors, with Lizard and Spock being additional choices.</p>
+          
+          <p>The name <b>Pro Slaps!</b> is from combining and rearranging the first couple of letters of each hand gesture.</p>
+          
+          <h4>Credits</h4>
+          
+          <p>Pro Slaps! was designed and developed by <a href="https://agskryp.com" target="_blank">Andrew Skrypnyk</a> using:</p>
+          
+          <ul>
+            <li>Vue v2.6.11</li>
+            <li>Bootstrap v4.4.1</li>
+            <li>Font Awesome v4.7.0</li>
+          </ul>            
+          
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            @click="closeInfo()"
+          >
+            Close
+          </button>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -4,7 +4,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Rules</h5>
-          
+
           <button
             type="button"
             class="close"
@@ -14,10 +14,10 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        
+
         <div class="modal-body">
           <p>Defeat the computer by selecting the greater hand gesture.</p>
-          
+
           <table class="table table-sm table-bordered">
             <thead class="table-active">
               <tr>
@@ -25,32 +25,32 @@
                 <th scope="col" colspan="2">Defeats</th>
               </tr>
             </thead>
-            
+
             <tbody>
               <tr>
                 <th scope="row">Rock</th>
                 <td>Lizard</td>
                 <td>Scissors</td>
               </tr>
-              
+
               <tr>
                 <th scope="row">Lizard</th>
                 <td>Spock</td>
                 <td>Paper</td>
               </tr>
-              
+
               <tr>
                 <th scope="row">Spock</th>
                 <td>Scissors</td>
                 <td>Rock</td>
               </tr>
-              
+
               <tr>
                 <th scope="row">Scissors</th>
                 <td>Paper</td>
                 <td>Lizard</td>
               </tr>
-              
+
               <tr>
                 <th scope="row">Paper</th>
                 <td>Rock</td>
@@ -58,16 +58,12 @@
               </tr>
             </tbody>
           </table>
-          
+
           <p>Choosing the same move as the machine will result in a draw.</p>
         </div>
-        
+
         <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            @click="rules()"
-          >
+          <button type="button" class="btn btn-secondary" @click="rules()">
             Close
           </button>
         </div>
