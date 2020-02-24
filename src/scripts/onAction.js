140b14a39043073fa8739f7ee5e651b85b121c8a
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-24 13:02:04 -0500

    Create + use defeat partial script

--- src/scripts/onAction.js
+++ src/scripts/onAction.js
@@ -1,8 +1,9 @@
 import victory from "@/scripts/results/victory.js";
 import draw from "@/scripts/results/draw.js";
+import defeat from "@/scripts/results/defeat.js";
 
 export default {
-  mixins: [victory, draw],
+  mixins: [victory, draw, defeat],
 
   methods: {
     invocation() {
@@ -29,30 +30,7 @@ export default {
 
       this.victory(hand);
       this.draw(hand);
-
-      if (
-        (hand == "hand-rock" && this.computerChoice == "hand-spock") ||
-        (hand == "hand-rock" && this.computerChoice == "hand-paper") ||
-        (hand == "hand-paper" && this.computerChoice == "hand-scissors") ||
-        (hand == "hand-paper" && this.computerChoice == "hand-lizard") ||
-        (hand == "hand-scissors" && this.computerChoice == "hand-spock") ||
-        (hand == "hand-scissors" && this.computerChoice == "hand-rock") ||
-        (hand == "hand-lizard" && this.computerChoice == "hand-scissors") ||
-        (hand == "hand-lizard" && this.computerChoice == "hand-rock") ||
-        (hand == "hand-spock" && this.computerChoice == "hand-lizard") ||
-        (hand == "hand-spock" && this.computerChoice == "hand-paper")
-      ) {
-        this.counterLoses++;
-        this.results = "Computer Wins";
-
-        if (hand == "hand-paper" && this.computerChoice == "hand-scissors") {
-          this.message = "Snip snip sinp!";
-        }
-
-        if (hand == "hand-paper" && this.computerChoice == "hand-lizard") {
-          this.message = "Gonna have to take a shower after this";
-        }
-      }
+      this.defeat(hand);
 
       clearTimeout(this.createPause);
       this.invocation();
---
+++ src/scripts/results/defeat.js
@@ -0,0 +1,30 @@
+export default {
+  methods: {
+    defeat(hand) {
+      if (
+        (hand == "hand-rock" && this.computerChoice == "hand-spock") ||
+        (hand == "hand-rock" && this.computerChoice == "hand-paper") ||
+        (hand == "hand-paper" && this.computerChoice == "hand-scissors") ||
+        (hand == "hand-paper" && this.computerChoice == "hand-lizard") ||
+        (hand == "hand-scissors" && this.computerChoice == "hand-spock") ||
+        (hand == "hand-scissors" && this.computerChoice == "hand-rock") ||
+        (hand == "hand-lizard" && this.computerChoice == "hand-scissors") ||
+        (hand == "hand-lizard" && this.computerChoice == "hand-rock") ||
+        (hand == "hand-spock" && this.computerChoice == "hand-lizard") ||
+        (hand == "hand-spock" && this.computerChoice == "hand-paper")
+      ) {
+        this.counterLoses++;
+        this.results = "Computer Wins";
+        this.message = "U Lose!  U Lose!"
+
+        if (hand == "hand-paper" && this.computerChoice == "hand-scissors") {
+          this.message = "Snip snip sinp!";
+        }
+
+        if (hand == "hand-paper" && this.computerChoice == "hand-lizard") {
+          this.message = "Gonna have to take a shower after this";
+        }
+      }
+    }
+  }
+};
