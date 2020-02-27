5c1001967aa6b7799b33cb9ab6230d263cd39550
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-02-27 12:43:37 -0500

    Stats reset wip

--- src/components/modals/Stats.vue
+++ src/components/modals/Stats.vue
@@ -1,62 +1,70 @@
 <template>
-  <div class="modal-container">
-    <div class="modal-dialog">
-      <div class="modal-content modal-lg">
-        <div class="modal-header">
-          <h3 class="modal-title">Stats</h3>
+  <div>
+    <div class="modal-container">
+      <div class="modal-dialog">
+        <div class="modal-content modal-lg">
+          <div class="modal-header">
+            <h3 class="modal-title">Stats</h3>
 
-          <button
-            type="button"
-            class="close"
-            aria-label="Close"
-            @click="close()"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
+            <button
+              type="button"
+              class="close"
+              aria-label="Close"
+              @click="close()"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
 
-        <div class="modal-body">
-          <div class="container">
-            <div class="row">
-              <b>Wins: {{ this.$parent.counterWins }}</b>
-            </div>
+          <div class="modal-body">
+            <div class="container">
+              <div class="row">
+                <b>Wins: {{ this.$parent.counterWins }}</b>
+              </div>
 
-            <div class="row">
-              <b>Draws: {{ this.$parent.counterDraws }}</b>
-            </div>
+              <div class="row">
+                <b>Draws: {{ this.$parent.counterDraws }}</b>
+              </div>
 
-            <div class="row">
-              <b>Loses: {{ this.$parent.counterLoses }}</b>
+              <div class="row">
+                <b>Loses: {{ this.$parent.counterLoses }}</b>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" @click="reset()">
-            Reset
-          </button>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" @click="reset()">
+              Reset
+            </button>
 
-          <button type="button" class="btn btn-secondary" @click="close()">
-            Close
-          </button>
+            <button type="button" class="btn btn-secondary" @click="close()">
+              Close
+            </button>
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="modal-dialog" v-if="showModalReset == true">
-      <div class="modal-content modal-lg">
-        <div class="modal-header">
-          <h3 class="modal-title">Are you sure?</h3>
-        </div>
+  <div class="modal-container" v-if="showModalReset == true">
+      <div class="modal-dialog">
+        <div class="modal-content modal-lg">
+          <div class="modal-header">
+            <h3 class="modal-title">Are you sure?</h3>
+          </div>
 
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" @click="deleteStats()">
-            Confirm
-          </button>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" @click="deleteStats()">
+              Confirm
+            </button>
 
-          <button type="button" class="btn btn-secondary" @click="closeReset()">
-            Close
-          </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="closeReset()"
+            >
+              Close
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -82,6 +90,10 @@ export default {
 
     reset: function() {
       this.showModalReset = true;
+    },
+
+    closeReset: function() {
+      this.showModalReset = false;
     }
   }
 };
